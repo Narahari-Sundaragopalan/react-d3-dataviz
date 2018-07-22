c7b77b20da72605ec2562987f4d6ec0e2e61d4ea
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-07-22 18:00:09 -0500

    Cleanup

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,11 @@
 import auth0 from 'auth0-js';
-
 import history from './history';
 
 export default class Auth {
-  // Please use your own credentials here
   auth0 = new auth0.WebAuth({
     domain: 'react-d3.auth0.com',
     clientID: 'UaqIHCSU29iEOMyrwMGXIY6Fgw-yCTzr',
     redirectUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/callback' : '',
-    // audience: 'https://divyanshu.auth0.com/userinfo',
-    responseType: 'token id_token',
     scope: 'openid'
   });
 
